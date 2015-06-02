28a090aa203d242f81bd4307b08d7f1804dc4308
Josh Wood <jwood@unlimitedsystems.com>
2015-06-02 12:58:42 -0400

    GitHub is dumb

---
+++ PorticoRegistration/Scripts/patient-angular.js
@@ -0,0 +1,10 @@
+﻿(function () {
+   var ngPatient = angular.module("ngRegistration", []);
+   ngPatient.controller("RegistrationController", function($scope, $http) {
+      $scope.tweets = [
+         { screenName: "Sumit", tweetText: "Test 1" },
+         { screenName: "Sumit", tweetText: "Test 2" }
+      ];
+   });
+})();
+
